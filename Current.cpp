2e1b07601cfebae3c338787dbd5e7d60523bca3d
MaxCypher05 <shegolev.maxim@gmail.com>
2022-12-12 19:30:02 +0300

    My_new_commit

---
+++ Current.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+void AM() {
+	int a, b, c;
+	cout << "Enter three number" << endl;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cout << "Result" << " " << (a + b + c) / 3;
+
+}
+
+int main() {
+	AM();
+	return  0;
+}
